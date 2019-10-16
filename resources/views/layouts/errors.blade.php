026638d9794df9ab4e04d771f1c45a07c0f2a5d7
Jim Callanta <mr.jimcallanta@gmail.com>
2019-10-16 15:46:03 +0800

    Add Error Pages

---
+++ resources/views/layouts/errors.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" prefix="og: http://ogp.me/ns#"><!--<![endif]-->
+<head>
+  <meta charset="UTF-8">
+  <title>Kredo Admin</title>
+  <meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:locale" content="ja_JP">
+<meta property="fb:admins" content="">
+<meta property="og:title" content="">
+<meta property="og:description" content="">
+<meta property="og:url" content="">
+<meta property="og:site_name" content="">
+<meta property="og:type" content="blog">
+<meta property="og:image" content="/assets/images/common/logo_ogp.png">
+
+<link rel="shortcut icon" href="/assets/images/common/favicon.ico">
+<link rel="apple-touch-icon-precomposed" href="/assets/images/common/apple-touch-icon-precomposed.png">
+
+<!-- Custom fonts for this template-->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css"/>
+
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+<script> document.documentElement.className = document.documentElement.className.replace("no-js","js");</script>
+
+</head>
+<body>
+  <div id="wrapper">
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+	    <div class="row p-5" style="background: #fff">
+	        <div class="col-md-12 text-center p-5 mt-5">
+				<img src="{{ asset('img/logo-kredo-sp.png') }}" alt="Kredo" class="mt-5 mb-3">
+        		@yield('content')
+	        </div>
+	    </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
