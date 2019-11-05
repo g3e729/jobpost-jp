8af8f15d3bac13d154c6f3a92e6e772d3957f033
Paul James Curacha <paul_james_curacha@ligph.com>
2019-11-05 14:47:24 +0800

    Modify pagination style

--- resources/views/admin/partials/pagination.blade.php
+++ resources/views/admin/partials/pagination.blade.php
@@ -1,17 +1,5 @@
-
-@if ($data instanceof \Illuminate\Pagination\LengthAwarePaginator)
-	{{ $data->appends(request()->except('page'))->links() }}
-@endif
-{{-- <nav aria-label="Page Navigation">
-	<ul class="pagination justify-content-center py-4">
-	  <li class="page-item disabled"><a class="page-link alt-font" href="#">前</a></li>
-	  
-	  <li class="page-item active"><a class="page-link alt-font" href="#">1</a></li>
-	  
-	  <li class="page-item "><a class="page-link alt-font" href="#">2</a></li>
-	  
-	  <li class="page-item "><a class="page-link alt-font" href="#">3</a></li>
-	  
-	  <li class="page-item"><a class="page-link alt-font" href="#">次</a></li>
-	</ul>
-</nav> --}}
+<div class="d-flex justify-content-center pt-4 pb-5">
+  @if ($data instanceof \Illuminate\Pagination\LengthAwarePaginator)
+    {{ $data->appends(request()->except('page'))->links() }}
+  @endif
+</div>
