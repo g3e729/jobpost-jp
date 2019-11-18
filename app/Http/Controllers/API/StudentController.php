8467542c48a042c3c646da7cb8a6ef047bf79f75
Jim Callanta <mr.jimcallanta@gmail.com>
2019-11-18 18:32:55 +0800

    API Companies [15%]

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -24,4 +24,38 @@ class StudentController extends BaseController
 	{
 		return $student->forApi();
 	}
+
+	public function update(Student $student, Request $request)
+	{
+        $student->update(
+            $request->except('_token', '_method', 'email', 'japanese_name', 'name')
+        );
+
+        $student->user()->update(
+            $request->only('email', 'japanese_name', 'name')
+        );
+
+        $works = $request->get('work_history');
+
+        if ($works && count(array_filter(array_flatten($works, 1)))) {
+        	$student->work_history()->delete();
+
+        	foreach($works as $work) {
+        		$work['started_at'] = Str::finish($work['started_at'], '-01');
+        		$work['ended_at'] = Str::finish($work['ended_at'], '-01');
+        		$student->work_history()->create($work);
+        	}
+        }
+
+        $educations = $request->get('education_history');
+
+        if ($educations && count(array_filter(array_flatten($educations, 1)))) {
+        	$student->education_history()->delete();
+
+        	foreach($educations as $education) {
+        		$education['graduated_at'] = Str::finish($education['graduated_at'], '-01');
+        		$student->education_history()->create($education);
+        	}
+        }
+	}
 }
