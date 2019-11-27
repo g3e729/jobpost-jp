62b300bc7da8d7ab71777d2db5a0178f78e0738e
Jim Callanta <mr.jimcallanta@gmail.com>
2019-11-27 16:20:58 +0800

    Search Filter Reset Button

--- resources/views/admin/companies/partials/search.blade.php
+++ resources/views/admin/companies/partials/search.blade.php
@@ -35,7 +35,7 @@
       <button type="submit" class="alt-font btn btn-primary btn-submit w-100">検索</button>
     </div>
     <div class="col-3">
-      <button type="reset" class="alt-font btn btn-secondary w-75">リセット</button>
+      <a href="{{ route('admin.companies.index') }}" class="alt-font btn btn-secondary w-75">リセット</a>
     </div>
   </div>
 </form>
