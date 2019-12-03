f66e6cc25fcc1138e239ac254afd26295cd67293
Jim Callanta <mr.jimcallanta@gmail.com>
2019-12-03 15:34:28 +0800

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('pageTitle') - {{ config('app.name') }}</title>
+  <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
   <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
 
