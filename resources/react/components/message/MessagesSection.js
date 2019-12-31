de3a268b3f9d38a0018998b68814917d1ccf79ab
Paul James Curacha <paul_james_curacha@ligph.com>
2019-12-31 11:55:04 +0800

    Add messages sidebar

--- resources/react/components/message/MessagesSection.js
+++ resources/react/components/message/MessagesSection.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const MessagesSection = _ => (
   <div className="messages-section">
-    Messages section
+    <h3 className="messages-section__header">株式会社Retty</h3>
+    <div className="messages-section__body">
+      body
+    </div>
   </div>
 );
 
--- resources/react/components/message/MessagesSidebar.js
+++ resources/react/components/message/MessagesSidebar.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import faker from 'faker';
 faker.locale = "ja";
+
+import Avatar from '../common/Avatar';
+import { state } from '../../constants/state';
 
 const dummyMessages = new Array(10)
   .fill(null)
   .map(e => {
     e = {};
     e.id = faker.random.uuid();
-    e.company = faker.company.companyName();
+    e.contact = faker.company.companyName();
     e.message = `Apply${faker.lorem.words(2)}`;
 
     return e;
   })
 
-const MessagesSidebar = _ => (
-  <aside className="messages-sidebar">
-    <h3 className="messages-sidebar__header">メッセージ</h3>
-    <ul className="messages-sidebar__chatroom">
-      { dummyMessages.map((item) => (
-        <li className="messages-sidebar__chatroom-item" key={item.id}>
-          {item.message}
-        </li>
-      ))}
-    </ul>
-  </aside>
-);
+const MessagesSidebar = _ => {
+  const [currentItem, setCurrentItem] = useState(0);
+
+  return (
+    <aside className="messages-sidebar">
+      <h3 className="messages-sidebar__header">メッセージ</h3>
+      <ul className="messages-sidebar__chatroom">
+        { dummyMessages.map((item, idx) => (
+          <li className={`messages-sidebar__chatroom-item ${idx === currentItem ? state.ACTIVE : ''}`}
+            onClick={_ => setCurrentItem(idx)} key={item.id}>
+            <div className="messages-sidebar__chatroom-item-left">
+              <div className="messages-sidebar__chatroom-item-avatar">
+                <Avatar className="avatar--message"
+                  style={{ backgroundImage: 'url("https://lorempixel.com/640/640/business/")' }}
+                />
+              </div>
+            </div>
+            <div className="messages-sidebar__chatroom-item-right">
+              <h4 className="messages-sidebar__chatroom-item-contact">
+                {item.contact}
+              </h4>
+              <p className="messages-sidebar__chatroom-item-message">
+                {item.message}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
 
 export default MessagesSidebar;
