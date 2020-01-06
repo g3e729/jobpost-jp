955ee3aa1fc15ad03b17fe14797bbc001ff1aa3d
Paul James Curacha <paul_james_curacha@ligph.com>
2020-01-06 10:13:06 +0800

    Add search tabs

--- resources/react/components/search/Search.js
+++ resources/react/components/search/Search.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { useLocation } from 'react-router-dom';
 
-const Search = _ => (
-  <div>
-    Search sidebar
-    Search content
-  </div>
-);
+import { state } from '../../constants/state';
+
+const Search = _ => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
+  const location = useLocation();
+  const urlParams = new URLSearchParams(location.search);
+
+  const handleTabChange = index => {
+    setTabIndex(index);
+  }
+
+  useEffect(_ => {
+    setSearchTerm(urlParams.get('query'));
+  }, [location]);
+
+  return (
+    <div className="search">
+      <Tabs className="search-tab" selectedIndex={tabIndex} onSelect={tabIndex => setTabIndex(tabIndex)}>
+        <TabList className="search-tab__list">
+          <Tab className="search-tab__list-item" selectedClassName={state.ACTIVE} onClick={_ => handleTabChange(0)}>すべて</Tab>
+          <Tab className="search-tab__list-item" selectedClassName={state.ACTIVE} onClick={_ => handleTabChange(1)}>会社</Tab>
+          <Tab className="search-tab__list-item" selectedClassName={state.ACTIVE} onClick={_ => handleTabChange(2)}>募集</Tab>
+        </TabList>
+
+        <TabPanel className="search-tab__panel">
+          <h3 className="search-tab__panel-title">で検索 {searchTerm}</h3>
+        </TabPanel>
+        <TabPanel className="search-tab__panel">
+          <h3 className="search-tab__panel-title">で検索 {searchTerm}</h3>
+        </TabPanel>
+        <TabPanel className="search-tab__panel">
+          <h3 className="search-tab__panel-title">で検索 {searchTerm}</h3>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+}
 
 export default Search;
