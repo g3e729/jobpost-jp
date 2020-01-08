1faec4ba0a9f6685bc57a9e481733ee613d9293c
Carlo Abasola <carlo_abasola@ligph.com>
2020-01-08 15:34:53 +0800

    Fix to top page button

--- resources/react/components/student/ScoutsPage.js
+++ resources/react/components/student/ScoutsPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Page from '../common/Page';
 import Heading from '../common/Heading';
 import FavoritesSection from './FavoritesSection';
+import PageUp from '../common/PageUp';
 
 const ScoutsPage = _ => {
   return (
@@ -16,6 +17,7 @@ const ScoutsPage = _ => {
           <FavoritesSection />
         </div>
       </div>
+      <PageUp />
     </Page>
   );
 }
