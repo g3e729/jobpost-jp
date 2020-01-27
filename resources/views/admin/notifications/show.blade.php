d9542bcde595227650c0f94ccb30bb337cb72f2b
Paul James Curacha <paul_james_curacha@ligph.com>
2020-01-27 11:23:04 +0800

    Apply modal on admin notification delete

--- resources/views/admin/notifications/show.blade.php
+++ resources/views/admin/notifications/show.blade.php
@@ -36,4 +36,45 @@
       </div>
     </div>
   </div>
+
+<div class="modal fade" id="js-delete-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalLabel">削除</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        [TODO] Are you sure you want to delete notification ？
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="alt-font btn btn-secondary" data-dismiss="modal">キャンセル</button>
+        <button id="js-modal-submit" type="button" class="alt-font btn btn-primary">確認する</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('js')
+  <script>
+    const deleteButton = document.querySelector('#js-item-delete');
+    const modalSubmit = document.querySelector('#js-modal-submit');
+    const modal = document.querySelector('#js-delete-modal');
+    let currTarget;
+
+    deleteButton.addEventListener('click', function(event) {
+      $(modal).modal('show');
+      currTarget = event.currentTarget.getAttribute('form');
+
+      event.preventDefault();
+    });
+
+    modalSubmit.addEventListener('click', function(event) {
+      $(modal).modal('hide');
+      document.querySelector(`#${currTarget}`).submit();
+    });
+  </script>
 @endsection
