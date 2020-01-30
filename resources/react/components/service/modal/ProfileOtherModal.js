9735c9366f530b2d7dcffaee91418723dd671163
MyznEiji <em711th@icloud.com>
2020-01-30 14:04:15 +0000

    rewrite: japanese

--- resources/react/components/service/modal/ProfileOtherModal.js
+++ resources/react/components/service/modal/ProfileOtherModal.js
@@ -136,7 +136,7 @@ const ProfileOtherModal = (props) => {
           <Button className="button--icon" onClick={_ => handleSubmit()}>
             <>
               <i className="icon icon-disk"></i>
-              セーブ
+              保存する
             </>
           </Button>
         </div>
